6cecc9094fc66d9b7dbdda0ba45ec26ff08fc633
李满满 <limanman@corp.netease.com>
2021-09-05 21:41:55 +0800

    :underage:

--- service_kombu/core/publish.py
+++ service_kombu/core/publish.py
@@ -6,11 +6,14 @@ from __future__ import annotations
 
 import typing as t
 
+from logging import getLogger
 from kombu import Producer as BaseProducer
 from service_core.core.context import WorkerContext
 from service_kombu.core.convert import from_context_to_headers
 from service_kombu.constants import DEFAULT_KOMBU_AMQP_HEADERS_MAPPING
 
+
+logger = getLogger(__name__)
 
 class Publisher(BaseProducer):
     """ AMQP通用发送类 """
@@ -25,11 +28,11 @@ class Publisher(BaseProducer):
         self.context = context
         super(Publisher, self).__init__(*args, **kwargs)
 
-    def publish(self, *args: t.Any, **kwargs: t.Any) -> None:
+    def publish(self, body: t.Any, **kwargs: t.Any) -> None:
         """ 发布消息
 
-        @param args  : 位置参数
-        @param kwargs: 命名参数
+        @param body: 发送内容
+        @param kwargs: 发布参数
         @return: None
         """
         headers = kwargs.get('headers', {})
@@ -40,4 +43,5 @@ class Publisher(BaseProducer):
         kwargs['headers'] = headers
         # 防止开启心跳后超时被服务端主动踢下线
         kwargs.setdefault('retry', True)
-        return super(Publisher, self).publish(*args, **kwargs)
+        logger.debug(f'publish message: {body} with {kwargs}')
+        return super(Publisher, self).publish(body, **kwargs)
